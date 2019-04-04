728882f6b4fe27b59117d44a273919940f4b0d90
ha <louishaiv@gmail.com>
2019-04-04 13:56:12 +0700

    branch queha admin

---
+++ LKmaytinh/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LKmaytinh.Models;
+using System.IO;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace LKmaytinh.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbMayTinhDataContext db = new dbMayTinhDataContext();
+
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
