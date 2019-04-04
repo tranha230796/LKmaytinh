f4cb74833a69cf479fa6ee2a8ddd7265c49740b8
ha <louishaiv@gmail.com>
2019-04-04 13:41:47 +0700

    version 1

---
+++ LKmaytinh/Controllers/GioHangController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LKmaytinh.Models;
+
+namespace LKmaytinh.Controllers
+{
+    public class GioHangController : Controller
+    {
+        dbMayTinhDataContext data = new dbMayTinhDataContext();
+        // GET: GioHang
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if(lstGiohang==null)
+            {
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        public ActionResult ThemGiohang(int iMaSP,string strURL)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.Find(n => n.iMaSP == iMaSP);
+            if(sanpham == null)
+            {
+                sanpham = new Giohang(iMaSP);
+                lstGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+        public ActionResult GioHang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if(lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "MayTinh");
+            }
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return View(lstGiohang);
+        }
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongSoLuong = lstGiohang.Sum(n => n.iSoluong);
+            }
+            return iTongSoLuong;
+        }
+        //Tinh tong tien
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongTien = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return iTongTien;
+        }
+        //Tao Partial view de hien thi thong tin gio hang
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return PartialView();
+        }
+       
+        public ActionResult CapnhatGiohang(int iMaSP, FormCollection f)
+        {
+
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMaSP == iMaSP);
+            
+            if (sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(f["txtSoluong"]);
+            }
+            return RedirectToAction("Giohang");
+        }
+        //Xoa Giohang
+        public ActionResult XoaGiohang(int iMaSP)
+        {
+            
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMaSP == iMaSP);
+            if (sanpham != null)
+            {
+                lstGiohang.RemoveAll(n => n.iMaSP == iMaSP);
+                return RedirectToAction("GioHang");
+
+            }
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "MayTinh");
+            }
+            return RedirectToAction("GioHang");
+        }
+        //Xoa tat ca thong tin trong Gio hang
+        public ActionResult XoaTatcaGiohang()
+        {
+           
+            List<Giohang> lstGiohang = Laygiohang();
+            lstGiohang.Clear();
+            return RedirectToAction("Index", "MayTinh");
+        }
+        
+        [HttpGet]
+        public ActionResult DatHang()
+        {
+            if (Session["Taikhoan"] == null || Session["Taikhoan"].ToString() == "")
+            {
+                return RedirectToAction("Dangnhap", "MayTinh");
+            }
+            if (Session["Giohang"] == null)
+            {
+                return RedirectToAction("Index", "MayTinh");
+            }
+
+            
+            List<Giohang> lstGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return View(lstGiohang);
+        }
+        [HttpPost]
+        public ActionResult DatHang(FormCollection collection)
+        {
+            DonDatHang dh = new DonDatHang();
+            KhachHang kh = (KhachHang)Session["Taikhoan"];
+            List<Giohang> gh = Laygiohang();
+            dh.MaKH = kh.MaKH;
+            dh.Ngaydat = DateTime.Now;
+            var ngaygiao = string.Format("{0:MM//dd//yyyy}",collection["Ngaygiao"]);
+            dh.Dathanhtoan = false;
+            data.DonDatHangs.InsertOnSubmit(dh);
+            data.SubmitChanges();
+            foreach(var item in gh)
+            {
+                ChiTietHD cthd = new ChiTietHD();
+                cthd.MaDH = dh.MaDH;
+                cthd.MaSP = item.iMaSP;
+                cthd.SL = item.iSoluong;
+
+            }
+            data.SubmitChanges();
+            Session["Giohang"] = null;
+            return RedirectToAction("Xacnhandh", "Giohang");
+        }
+        public ActionResult Xacnhandh()
+        {
+            return View();
+        }
+    }
+}
---
+++ LKmaytinh/Controllers/loaispController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LKmaytinh.Models;
+
+namespace LKmaytinh.Controllers
+{
+    public class loaispController : Controller
+    {
+        //GET: loai
+       dbMayTinhDataContext db = new dbMayTinhDataContext();
+        public ActionResult loaisp()
+        {
+            return PartialView(db.Loais.Take(12).ToList());
+        }
+        //public ActionResult loaisp()
+        //{
+        //    var loaisp = from Loai in db.Loais select Loai;
+        //    return PartialView(loaisp);
+        //}
+    }
+}
