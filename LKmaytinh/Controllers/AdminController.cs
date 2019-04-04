c730ebfd3c143d221c247149835a3b0ebf94c76a
dohoangvu <32243484+dohoangvu@users.noreply.github.com>
2019-04-04 14:07:36 +0700

    add AdminController.cs

---
+++ LKmaytinh/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LKmaytinh.Models;
+using System.IO;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace LKmaytinh.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbMayTinhDataContext db = new dbMayTinhDataContext();
+
+
+        // GET: Admin
+        public ActionResult SanPham()
+        {
+            if (Session["Taikhoanadmin"] == null || Session["Taikhoanadmin"].ToString() == "")
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            return View(db.SanPhams.ToList());
+        }
+    }
+}
